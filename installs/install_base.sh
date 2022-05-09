04f7a0e1515c10488cc633218e30973d52296b4d
Michael Linhardt <linhardt.michael@gmail.com>
2022-05-09 21:01:03 +0700

    update services settings

--- installs/install_base.sh
+++ installs/install_base.sh
@@ -1,6 +1,10 @@
 pacman -S base-devel git vim sudo iwd netctl networkmanager grub efibootmgr intel-ucode alsa alsa-plugins alsa-utils htop neofetch man-db man-pages texinfo dhcpcd
 sudo systemctl enable --now iwd
 sudo systemctl enable --now dhcpcd
+sudo cp ~/workspace_archlinux/services/iwd.service /usr/lib/systemd/system/iwd.service
+sudo cp ~/workspace_archlinux/services/getty@.service /usr/lib/systemd/system/getty@.service
 echo "Done!"
 echo "-> Need to uncomment group wheel with visudo command"
 echo "-> edit /etc/default/grub, set GRUB_TIMEOUT=0, rebuild grub config (alias: updgrub)"
+echo "-> added 3sc delay to iwd service at start (prevent start while wifi card not ready)"
+echo "-> added getty file to skip login on computer start"
