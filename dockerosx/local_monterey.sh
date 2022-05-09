403efb56c3c071d953b82bc9ac38977004f34cd6
Michael Linhardt <linhardt.michael@gmail.com>
2022-05-10 01:14:48 +0700

    upd

--- dockerosx/local_monterey.sh
+++ dockerosx/local_monterey.sh
@@ -1,12 +1,10 @@
-xhost + && docker run \
+xhost + && docker run -it \
     --memory 14G \
-    -e RAM=14 \
     --memory-swap 20G \
-    -e CPU_STRING=16 \
     --cpus=16 \
-    -it \
     --device /dev/kvm \
     -p 50922:10022 \
+    -v "${PWD}/osximg/mac_hdd_ng.img:/image" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e "DISPLAY=${DISPLAY:-:0.0}" \
-    sickcodes/docker-osx:monterey
+    sickcodes/docker-osx:naked
