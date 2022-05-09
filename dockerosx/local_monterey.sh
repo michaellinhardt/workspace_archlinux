614b6843c0bd931c19e0338cef6ebf8c33ed6f8c
Michael Linhardt <linhardt.michael@gmail.com>
2022-05-10 01:41:22 +0700

    upd

--- dockerosx/local_monterey.sh
+++ dockerosx/local_monterey.sh
@@ -4,6 +4,9 @@ xhost + && docker run -it \
     --cpus=16 \
     --device /dev/kvm \
     -p 50922:10022 \
+    -e GENERATE_UNIQUE=true \
+    -e WIDTH=1600 \
+    -e HEIGHT=900 \
     -v "${PWD}/osximg/mac_hdd_ng.img:/image" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e "DISPLAY=${DISPLAY:-:0.0}" \
