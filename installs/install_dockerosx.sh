14d5c29cec8faf296b197297f7c235b0d5354268
Michael Linhardt <linhardt.michael@gmail.com>
2022-05-09 21:03:53 +0700

    fix service docker setup

--- installs/install_dockerosx.sh
+++ installs/install_dockerosx.sh
@@ -3,5 +3,5 @@ sudo systemctl enable --now libvirtd
 sudo systemctl enable --now virtlogd
 echo 1 | sudo tee /sys/module/kvm/parameters/ignore_msrs
 sudo modprobe kvm
-sudo systemctl --enable --now docker
+sudo systemctl enable --now docker
 sudo usermod -aG docker,libvirt,kvm "${USER}"
