b0e7b7548644edc604dfe37ab6cef03c1f2857ef
Michael Linhardt <linhardt.michael@gmail.com>
2022-05-23 06:04:23 +0700

    upd

--- scripts/installs/install_awesome.sh
+++ scripts/installs/install_awesome.sh
@@ -1,4 +1,4 @@
-sudo pacman -S awesome
+sudo pacman -S xorg-server xorg-xinit xterm awesome
 mkdir -p ~/.config/awesome/
 cp $PATH_WORKSPACE/configs/xinitrc/xinitrc_awesome /home/teazyou/.xinitrc
 cp /home/teazyou/workspace_archlinux/configs/awesome/rc_replacer.lua /home/teazyou/.config/awesome/rc.lua
