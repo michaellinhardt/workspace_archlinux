da1d306856bcce0828eaa1964e1aaca03a79d4d5
Michael Linhardt <linhardt.michael@gmail.com>
2022-05-23 12:31:11 +0700

    upd

--- scripts/installs/install_awesome.sh
+++ scripts/installs/install_awesome.sh
@@ -1,4 +1,10 @@
-sudo pacman -S xorg-server xorg-xinit xterm awesome
-mkdir -p ~/.config/awesome/
-cp $PATH_WORKSPACE/configs/xinitrc/xinitrc_awesome /home/teazyou/.xinitrc
-cp /home/teazyou/workspace_archlinux/configs/awesome/rc_replacer.lua /home/teazyou/.config/awesome/rc.lua
+sudo pacman -S xorg-server xorg-xinit xterm awesome rofi papirus-icon-theme
+yay -S pamixer librewolf mpc mpd scrot unclutter xsel slock ttf-roboto i3lock xclip materia-theme lxappearance xorg-xbacklight xfce4-power-manager glib lain-git
+cd /home/teazyou/srcpkgs
+git clone --depth 1 --recursive https://github.com/lcpz/awesome-copycats.git
+mv -bv awesome-copycats/* /home/teazyou/.config/awesome && rm-rf awesome-copycats
+mkdir -p /home/teazyou/.config/awesome/
+sudo cp $PATH_WORKSPACE/configs/xinitrc/xinitrc_awesome /home/teazyou/.xinitrc
+sudo cp $PATH_WORKSPACE/configs/awesome/rc_replacer.lua /home/teazyou/.config/awesome/rc.lua
+mkdir -p /home/teazyou/.config/rofi/
+sudo cp $PATH_WORKSPACE/configs/awesome/rofi/config.rasi /home/teazyou/.config/rofi/config.rasi
--- scripts/installs/install_base.sh
+++ scripts/installs/install_base.sh
@@ -6,7 +6,7 @@ mkdir /home/teazyou/srcpkgs
 mkdir /home/teazyou/dev
 
 # base package
-sudo pacman -S base-devel git curl vim sudo pass xdg-utils tig iwd netctl networkmanager grub efibootmgr intel-ucode alsa alsa-plugins alsa-utils htop neofetch man-db man-pages texinfo dhcpcd zsh python ruby npm java-runtime-common java-environment-common mysql rclone
+sudo pacman -S base-devel git curl vim sudo pass xdg-utils tig iwd netctl networkmanager grub efibootmgr intel-ucode alsa alsa-plugins alsa-utils htop neofetch man-db man-pages texinfo dhcpcd zsh python ruby npm java-runtime-common java-environment-common mysql rclone util-linux php pulseaudio pulseaudio-alsa nvidia pavucontrol pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-zeroconf gimp bluez bluez-utils
 
 # git config
 sudo cp /home/teazyou/workspace_archlinux/configs/git/gitconfig /home/teazyou/.gitconfig
@@ -21,7 +21,7 @@ sudo yay -Syu
 yay --save --answerdiff None --answerclean None --removemake
 
 # yay packages
-yay -S franz code spotify firefox google-chrome nvm watchman zoom postman nordvpn bitwarden
+yay -S franz code spotify firefox google-chrome nvm watchman zoom postman nordvpn bitwarden pnmixer
 
 # git credential manager ( and pass config )
 yay -S git-credential-manager-core-bin
@@ -37,7 +37,12 @@ sudo systemctl enable --now dhcpcd
 sudo cp /home/teazyou/workspace_archlinux/configs/services/iwd.service /usr/lib/systemd/system/iwd.service
 
 # service to auto login when reboot
-sudo cp /home/teazyou/workspace_archlinux/configs/services/getty@.service /usr/lib/systemd/system/getty@.service
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
+sudo cp /home/teazyou/workspace_archlinux/configs/services/override.conf /etc/systemd/system/getty@tty1.service.d/
+
+# bash
+cp /home/teazyou/workspace_archlinux/configs/bashrc/bashrc_replacer /home/teazyou/.bashrc
+cp /home/teazyou/workspace_archlinux/configs/bashrc/bash_profile_replacer /home/teazyou/.bash_profile
 
 # oh-my-zsh
 cp /home/teazyou/workspace_archlinux/configs/zshrc/zshrc_replacer /home/teazyou/.zshrc
@@ -48,6 +53,7 @@ fi
 echo "Done!"
 echo "-> Need to uncomment group wheel with visudo command"
 echo "-> edit /etc/default/grub, set GRUB_TIMEOUT=0, rebuild grub config (alias: updgrub)"
+echo "-> edit /etc/default/grub, add 'snd_hda_intel.dmic_detect=0' at the end of GRUB_CMDLINE_LINUX_DEFAULT to enable sound.."
 echo "-> added 3sc delay to iwd service at start (prevent start while wifi card not ready)"
 echo "-> added getty file to skip login on computer start"
 
